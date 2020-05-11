c58149fd8c205ecb2afc8296a51e77499d17f272
dtsylyuryk <dtsylyuryk@tiltingpoint.com>
2020-05-11 12:09:32 +0300

    Refactor greedy search service/routes

--- server/api/routes/greedy.routes.js
+++ server/api/routes/greedy.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 
-const { getGreedyDelay } = require('../services/greedy.service');
+const { getGreedyResult } = require('../services/greedy.service');
 
 const router = Router();
 
 router.post('/', (req, res) => {
   const { numberOfJobs } = req.body;
-  const orderedJobs = getGreedyDelay(numberOfJobs);
+  const orderedJobs = getGreedyResult(numberOfJobs);
 
   res.send(orderedJobs);
 });
