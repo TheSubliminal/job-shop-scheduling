7a4271275e37a0172023018bbbbcada26a15d7f3
Michael Fridrikhson <fridrihson.1701@gmail.com>
2020-05-21 01:59:47 +0300

    Add ability to select multiple algorithms

--- client/src/components/AlgorithmDashboard/index.jsx
+++ client/src/components/AlgorithmDashboard/index.jsx
@@ -23,8 +23,6 @@ import { positiveError, requiredError, maxError } from '../../config/errorMessag
 
 import styles from './styles.module.scss';
 
-const algorithmKeys = Object.values(algorithms).map(({ key }) => key);
-
 const jobSchema = Yup.object().shape({
   duration: Yup.number()
     .positive(positiveError)
@@ -43,6 +41,9 @@ const inputDataSchema = Yup.object().shape({
     then: Yup.array().of(jobSchema).min(1).required(requiredError)
   }),
   isRandom: Yup.boolean(),
+  greedy: Yup.boolean(),
+  schildFredman: Yup.boolean(),
+  aco: Yup.boolean(),
   numOfRandomJobs: Yup.number().when('isRandom', {
     is: true,
     then: Yup.number()
@@ -51,7 +52,6 @@ const inputDataSchema = Yup.object().shape({
       .integer()
       .required(requiredError)
   }),
-  algorithm: Yup.mixed().oneOf(algorithmKeys).required(),
   numOfAnts: Yup.number().when('algorithm', {
     is: algorithms.aco.key,
     then: Yup.number()
@@ -97,7 +97,7 @@ class AlgorithmDashboard extends React.Component {
 
   onSubmit = (values) => {
     const {
-      algorithm,
+      aco,
       jobs,
       isRandom,
       numOfRandomJobs,
@@ -107,7 +107,11 @@ class AlgorithmDashboard extends React.Component {
       pheromoneEvaporationCoef
     } = values;
 
-    const algorithmInfo = { algorithm };
+    const selectedAlgorithms = Object.values(algorithms)
+      .filter(({ key }) => values[key])
+      .map(({ key }) => key);
+
+    const algorithmInfo = { algorithms: selectedAlgorithms };
 
     if (isRandom) {
       algorithmInfo.numOfRandomJobs = numOfRandomJobs;
@@ -115,7 +119,7 @@ class AlgorithmDashboard extends React.Component {
       algorithmInfo.jobs = jobs.map((job, index) => ({ ...job, id: index + 1 }));
     }
 
-    if (algorithm === algorithms.aco.key) {
+    if (aco) {
       algorithmInfo.numOfAnts = numOfAnts;
       algorithmInfo.pheromoneSignificanceCoef = pheromoneSignificanceCoef;
       algorithmInfo.heuristicSignificanceCoef = heuristicSignificanceCoef;
@@ -154,9 +158,11 @@ class AlgorithmDashboard extends React.Component {
             <Formik
               initialValues={{
                 jobs: [{ duration: defaults.duration, deadline: defaults.deadline }],
+                [algorithms.greedy.key]: true,
+                [algorithms.schildFredman.key]: false,
+                [algorithms.aco.key]: false,
                 isRandom: false,
                 numOfRandomJobs: defaults.numOfRandomJobs,
-                algorithm: defaults.algorithm,
                 numOfAnts: defaults.numOfAnts,
                 pheromoneSignificanceCoef: defaults.pheromoneSignificanceCoef,
                 heuristicSignificanceCoef: defaults.heuristicSignificanceCoef,
@@ -166,28 +172,33 @@ class AlgorithmDashboard extends React.Component {
               validateOnBlur
               onSubmit={this.onSubmit}
             >
-              {({ values, errors, handleSubmit, setFieldValue }) => (
-                <>
-                  <DataForm
-                    jobs={values.jobs}
-                    isRandom={values.isRandom}
-                    numOfRandomJobs={values.numOfRandomJobs}
-                    setJobs={setFieldValue.bind(null, 'jobs')}
-                  />
-                  <AlgorithmChoice />
-                  {values.algorithm === algorithms.aco.key && <ACOAdditionalData />}
-                  <div>
-                    <Button
-                      disabled={!!Object.keys(errors).length}
-                      onClick={handleSubmit}
-                      color='primary'
-                      variant='contained'
-                    >
-                      Submit
-                    </Button>
-                  </div>
-                </>
-              )}
+              {({ values, errors, handleSubmit, setFieldValue }) => {
+                const errorsExist = !!Object.keys(errors).length;
+                const areAlgorithmsSelected = values.greedy || values.schildFredman || values.aco;
+
+                return (
+                  <>
+                    <DataForm
+                      jobs={values.jobs}
+                      isRandom={values.isRandom}
+                      numOfRandomJobs={values.numOfRandomJobs}
+                      setJobs={setFieldValue.bind(null, 'jobs')}
+                    />
+                    <AlgorithmChoice />
+                    {values.aco && <ACOAdditionalData />}
+                    <div>
+                      <Button
+                        disabled={errorsExist || !areAlgorithmsSelected}
+                        onClick={handleSubmit}
+                        color='primary'
+                        variant='contained'
+                      >
+                        Submit
+                      </Button>
+                    </div>
+                  </>
+                );
+              }}
             </Formik>
           </ExpansionPanelDetails>
         </ExpansionPanel>
