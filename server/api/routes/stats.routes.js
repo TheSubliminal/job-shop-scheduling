177117f6cfa1c38eb6cd51f7f2074021cbb88aa3
dtsylyuryk <dtsylyuryk@tiltingpoint.com>
2020-05-24 13:57:06 +0300

    Add time complexity service

---
+++ server/api/routes/stats.routes.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+
+const { getTimeComplexityResults } = require('../services/timeCompexity.service');
+
+const router = Router();
+
+router
+  .post('/time-complexity', (req, res) => {
+    const result = getTimeComplexityResults(req.body);
+
+    res.send(result);
+  })
+  .post('/job-duration', (req, res) => {
+    const result = getResult(req.body);
+
+    res.send(result);
+  })
+  .post('/num-of-jobs', (req, res) => {
+    const result = getResult(req.body);
+
+    res.send(result);
+  });
+
+module.exports = router;
